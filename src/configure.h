a9f7e5f0245d2b23cc1daf655a34a5aee83224d0
3pleL <3pleL>
2021-03-02 22:20:22 +0100

    Added basic support for the creality ender 3 pro (melzi) mainboard. It is based on the sanguinolulu pin definitions.

--- src/configure.h
+++ src/configure.h
@@ -56,6 +56,7 @@
 #define BOARD_CNCV3        7  // Mega2560 + CNC Shield v3
 #define BOARD_ESP32        8  // ESP32 + Marginally Clever Polargraph PCB.
 #define BOARD_SKRPRO1_2    9  // SKR Pro 1.2
+#define BOARD_ENDER3      10  // Ender 3 Pro Melzi Board
 
 // default value 
 // !! this is now handeled via pio.ini !!
@@ -96,6 +97,7 @@
 #include "boards/cncv3.h"
 #include "boards/esp32.h"
 #include "boards/skrpro.h"
+#include "boards/ender3.h"
 
 #include "config_motors.h"
 
